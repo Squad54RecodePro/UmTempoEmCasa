ed04146829433a59ee605ebee60c248545f7d35f
John. Oliveira <jcleytonuber@gmail.com>
2021-12-27 08:23:20 -0300

    Adicionando tabela Reserva, faltando finalizar

---
+++ Models/Reserva.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmTempoEmCasa.Models
+{
+    public class Reserva
+    {
+
+    
+        [Key]
+        public int Id { get; set; }
+
+        public virtual Refugiado Refugiado { get; set; }
+        public virtual Anfitriao Anfitriao { get; set; }
+
+        [Required(ErrorMessage ="Defina a Data de Inicio do Contrato")]
+        [Display(Name ="Data de Inicio")]
+        public DateTime DateInicio { get; set; }
+
+        [Required(ErrorMessage ="Defina a Data de Termino do Contrato")]
+        [Display(Name ="Data de Encerramento")]
+        public DateTime DataFim { get; set; }
+
+
+    }
+}
